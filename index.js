0bbc977a1500179e402fb4bb986fd41f21869f5c
mesandeepmehta <119277325+mesandeepmehta@users.noreply.github.com>
2023-03-15 16:39:26 +0530

    Some added functionality with messages

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 let userMessage = document.getElementById("game-info")
 let cardSum = document.getElementById("card-sum")
 let userCards = document.getElementById("cards")
-let firstCard = 1
-let secondCard = 1
+let impMsg =  document.getElementById("imp-msg")
+let firstCard = 11
+let secondCard = 10
 let wins = 0
 let loss = 0
 let matches = wins + loss
 let cardArray = [firstCard , secondCard]
 let sum = firstCard + secondCard
-let gameOver =  false               //This is to show that by default user is not a loser
+let gameOver =  false  //This is to show that by default user is not a loser
 
+ //initially this button is hidden
+document.getElementById("new-card-btn").style.visibility="hidden"    
+
+
+function startGame(){
+    renderGame()
+}
 function renderGame(){
     userCards.innerText="Your cards: "
     //for loop to dynamically add new cards when user draws it
@@ -17,20 +25,31 @@ function renderGame(){
         userCards.textContent +=  cardArray[i] + " "
     }      
     cardSum.innerText="The sum is: "+ sum
+
+    document.getElementById("new-card-btn").style.visibility="visible"  //to make new card button visible
+
     if (sum > 21){
         userMessage.innerText="Game  over You lose"
         loss+=1
+        gameOver=true
+         //to make new card button invisible once it is not needed
         document.getElementById("new-card-btn").style.visibility="hidden"
+        impMsg.textContent= " you lose"
     }
     else if(sum === 21){
         userMessage.innerHTML="You got blackjack"
+        impMsg.innerText=" You Win"
         win+=1
     }
     else {
         userMessage.innerHTML="Do you want to draw another card"
     }
     document.getElementById("play-btn").style.visibility="hidden"
+    
 }
+
+
+
 //this function is to draw a new card
 function newCard(){
     let thirdCard = 3
